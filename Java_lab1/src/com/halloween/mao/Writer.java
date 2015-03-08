26368aa2a6f66e76ce2d7a716d435bb652ce7efe
Shaco <maoshaco@gmail.com>
2015-03-08 10:07:20 +0300

    Writer

---
+++ Java_lab1/src/com/halloween/mao/Writer.java
@@ -0,0 +1,35 @@
+package com.halloween.mao;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by Mao on 3/7/2015.
+ */
+public class Writer {
+    public static void write(String fileName, String text) {
+
+        File file = new File(fileName);
+
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fileWriter;
+            fileWriter = new FileWriter(file.getAbsoluteFile(), true);
+            BufferedWriter out;
+            out = new BufferedWriter(fileWriter);
+
+            try {
+                out.append(text);
+            } finally {
+                out.close();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
