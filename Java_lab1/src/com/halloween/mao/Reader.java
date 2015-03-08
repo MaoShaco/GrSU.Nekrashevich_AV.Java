13bc814a1f4b83d7bf5197d26c5196380bafdd55
Shaco <maoshaco@gmail.com>
2015-03-08 10:07:15 +0300

    Reader

---
+++ Java_lab1/src/com/halloween/mao/Reader.java
@@ -0,0 +1,54 @@
+package com.halloween.mao;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Reader {
+
+    private String path;
+
+    public Reader(String fileName) throws FileNotFoundException {
+    {
+        Exists(fileName);
+        this.path = fileName;
+    }
+    }
+
+    private void Exists(String fileName) throws FileNotFoundException {
+        File DataBase = new File(fileName);
+        if (!DataBase.exists()){
+            throw new FileNotFoundException(DataBase.getName());
+        }
+    }
+
+
+    public String GetStrings(int readFrom, int rowsToRead ) {
+
+        StringBuilder list = new StringBuilder();
+
+        try {
+            BufferedReader in = new BufferedReader(new FileReader(this.path));
+            try {
+                String bufferLine;
+                int count = 0;
+                while ((bufferLine = in.readLine()) != null && count - readFrom + 1 != rowsToRead) {
+                    count++;
+                    if (count >= readFrom - 1) {
+                        list.append(bufferLine);
+                        list.append('\n');
+                    }
+                }
+            } finally {
+                in.close();
+            }
+        } catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+        return list.toString();
+    }
+}
