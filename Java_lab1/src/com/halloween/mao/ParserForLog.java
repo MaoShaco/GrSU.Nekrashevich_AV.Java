ac15558c3263c56ec50f1f7a52df94595ea7ce6d
Shaco <maoshaco@gmail.com>
2015-03-08 10:07:09 +0300

    Parser for Requests
    Parser for class AccessLog

---
+++ Java_lab1/src/com/halloween/mao/ParserForLog.java
@@ -0,0 +1,22 @@
+package com.halloween.mao;
+
+/**
+ * Created by Mao on 3/7/2015.
+ */
+public class ParserForLog {
+
+    public static AccessLog  getAccessLog(String str){
+        AccessLog Buffer = new AccessLog();
+
+        String[] buffer = str.split("\"");
+        Buffer.setRequest(buffer[1]);
+        String[] secondPart = buffer[0].split("- ");
+        Buffer.setLocalHost(secondPart[0].trim());
+        Buffer.setTimestamp(secondPart[2]);
+        secondPart = buffer[2].split(" ");
+        Buffer.setReplyCode(Integer.parseInt(secondPart[1]));
+        Buffer.setReplyBytes(Integer.parseInt(secondPart[2]));
+
+        return Buffer;
+    }
+}
